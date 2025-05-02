d4aaab5787908e99f1291b0308a506d53d294b10
Andi Koller <34129969+Lassur@users.noreply.github.com>
2025-05-02 21:45:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+fetch('https://alpsteindb.onrender.com/api/eintraege')
+  .then(response => response.json())
+  .then(data => {
+    const root = document.getElementById('root');
+    root.innerHTML = '<h1>Gipfelbuch-Einträge</h1>' +
+      '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+  })
+  .catch(error => {
+    document.getElementById('root').innerHTML = '<p>Fehler beim Laden der Daten 😢</p>';
+    console.error(error);
+  });
