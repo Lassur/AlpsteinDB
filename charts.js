c7a20824684b9f95d972c0199401d493012273eb
Andi Koller <34129969+Lassur@users.noreply.github.com>
2025-05-05 16:24:54 +0200

    Delete charts.js

--- charts.js
+++
@@ -1,9 +0,0 @@
-const API_BASE = "https://alpsteindb.onrender.com/api";
-let chart, saisonChart, routeChart, topdayChart;
-
-function buildUrl(params, type = "jahr") {
-  const qs = new URLSearchParams(params);
-  return API_BASE + "/chart/" + type + "?" + qs.toString();
-}
-
-// ... [Gekürzter JS-Code aus vorheriger Nachricht, siehe oben] ...
